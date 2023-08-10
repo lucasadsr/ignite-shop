88f462eb7b691665a4ac0d3f4dbc937dd9f06359
Lucas Araújo <lucasaraujodesa@hotmail.com>
2023-08-10 09:19:47 -0300

    feat: base app (1.0)

---
+++ src/pages/api/checkout.ts
@@ -0,0 +1,31 @@
+import { stripe } from "@/src/lib/stripe";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { priceId } = req.body
+
+  if (req.method !== 'POST') {
+    res.status(405)
+  }
+
+  if (!priceId) {
+    res.status(400).json({error: 'price not found.'})
+  }
+
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
+  const cancelUrl = `${process.env.NEXT_URL}/`
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+    mode: 'payment',
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      }
+    ],
+  })
+
+  return res.status(201).json({ checkouturl: checkoutSession.url })
+}
