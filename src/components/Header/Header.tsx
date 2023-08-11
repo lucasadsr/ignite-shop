486d4db0739d7b83eb757a627c054f137577c6df
Lucas Araújo <lucasaraujodesa@hotmail.com>
2023-08-11 15:28:47 -0300

    fix: logo is now a link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,13 +4,16 @@ import { HeaderContainer } from './styles'
 import { Handbag } from 'phosphor-react'
 import { useShoppingCart } from 'use-shopping-cart'
 import { Cart } from '../Cart'
+import Link from 'next/link'
 
 export function Header() {
   const { cartCount, handleCartClick } = useShoppingCart()
 
   return (
     <HeaderContainer>
-      <Image src={logoImage} alt='' />
+      <Link href={'/'}>
+        <Image src={logoImage} alt='' />
+      </Link>
       <button onClick={() => handleCartClick()} >
         <Handbag width={24} height={24} color='#8D8D99' weight='bold' />
         {cartCount > 0 && <span>{cartCount}</span>}
